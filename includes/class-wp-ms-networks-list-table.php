10df89ff10646bc4d360cfeb9e6f6acf7ed4c8df
ddean <ddean@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-01-09 20:57:43 +0000

    Parameterized more queries; updated Networks list to a WP_List_Table derivative to fit current code standards
    git-svn-id: https://plugins.svn.wordpress.org/wp-multi-network/trunk@487174 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-wp-ms-networks-list-table.php
@@ -0,0 +1,265 @@
+<?php
+/**
+ * Networks List Table class.
+ *
+ * @package WPMN
+ * @since 1.3
+ * @access private
+ */
+class WP_MS_Networks_List_Table extends WP_List_Table {
+
+	function __construct() {
+		parent::__construct( array(
+			'plural' => 'networks',
+		) );
+	}
+
+	function ajax_user_can() {
+		return current_user_can( 'manage_network_options' );
+	}
+
+	function prepare_items() {
+		global $s, $mode, $wpdb, $current_site;
+
+		$mode = ( empty( $_REQUEST['mode'] ) ) ? 'list' : $_REQUEST['mode'];
+
+		$per_page = $this->get_items_per_page( 'networks_per_page' );
+
+		$pagenum = $this->get_pagenum();
+
+		$s = isset( $_REQUEST['s'] ) ? stripslashes( trim( $_REQUEST[ 's' ] ) ) : '';
+		$wild = '';
+		if ( false !== strpos($s, '*') ) {
+			$wild = '%';
+			$s = trim($s, '*');
+		}
+
+		$like_s = esc_sql( like_escape( $s ) );
+
+		$query = "SELECT {$wpdb->site}.*, meta1.meta_value as sitename, meta2.meta_value as network_admins, COUNT({$wpdb->blogs}.blog_id) as blogs, {$wpdb->blogs}.path as blog_path
+						FROM {$wpdb->site}
+					LEFT JOIN {$wpdb->blogs}
+						ON {$wpdb->blogs}.site_id = {$wpdb->site}.id {$search_conditions}
+					LEFT JOIN {$wpdb->sitemeta} meta1
+					ON
+						meta1.meta_key = 'site_name' AND
+						meta1.site_id = {$wpdb->site}.id
+					LEFT JOIN {$wpdb->sitemeta} meta2 
+					ON
+						meta2.meta_key = 'site_admins' AND
+						meta2.site_id = {$wpdb->site}.id";
+		
+
+		if ( empty($s) ) {
+			// Nothing to do.
+		} else {
+			if ( is_numeric($s) && empty( $wild ) ) {
+				
+				$query .= " WHERE ( {$wpdb->site}.site_id = '{$like_s}' )";
+				
+			} elseif ( is_subdomain_install() ) {
+				
+				$blog_s = str_replace( '.' . $current_site->domain, '', $like_s );
+				$blog_s .= $wild . '.' . $current_site->domain;
+				$query .= " WHERE ( {$wpdb->site}.domain LIKE '$blog_s' ) ";
+				
+			} else {
+				
+				if ( $like_s != trim('/', $current_site->path) )
+					$blog_s = $current_site->path . $like_s . $wild . '/';
+				else
+					$blog_s = $like_s;
+				$query .= " WHERE  ( {$wpdb->site}.path LIKE '$blog_s' )";
+				
+			}
+		}
+
+		$order_by = isset( $_REQUEST['orderby'] ) ? $_REQUEST['orderby'] : '';
+		$order_by = null;
+		
+		if ( isset( $order_by ) ) {
+			$order = ( isset( $_REQUEST['order'] ) && 'DESC' == strtoupper( $_REQUEST['order'] ) ) ? "DESC" : "ASC";
+			$query .= $order;
+		}
+
+		$total = $wpdb->get_var( str_replace( 'SELECT *', 'SELECT COUNT( site_id )', $query ) );
+
+		$query .= " GROUP BY {$wpdb->site}.id";
+
+		$query .= " LIMIT " . intval( ( $pagenum - 1 ) * $per_page ) . ", " . intval( $per_page );
+		
+//		echo $query;
+		
+		$this->items = $wpdb->get_results( $query, ARRAY_A );
+
+		$this->set_pagination_args( array(
+			'total_items' => $total,
+			'per_page' => $per_page,
+		) );
+	}
+
+	function no_items() {
+		_e( 'No networks found.' );
+	}
+
+	function get_bulk_actions() {
+		$actions = array();
+		if ( current_user_can( 'delete_sites' ) )
+			$actions['delete'] = __( 'Delete' );
+		$actions['spam'] = _x( 'Mark as Spam', 'site' );
+		$actions['notspam'] = _x( 'Not Spam', 'site' );
+
+		return $actions;
+	}
+
+	function pagination( $which ) {
+		global $mode;
+
+		parent::pagination( $which );
+
+		if ( 'top' == $which )
+			$this->view_switcher( $mode );
+	}
+
+	function get_columns() {
+		$networks_columns = array(
+			'cb'			=> '<input type="checkbox" />',
+			'sitename'		=> __( 'Site Name' ),
+			'domain' 		=> __( 'Domain' ),
+			'path'   		=> __( 'Path' ),
+			'blogs'  		=> __( 'Sites' ),
+			'admins' 		=> __( 'Network Admins' )
+		);
+
+		$networks_columns = apply_filters( 'wpmn_networks_columns', $networks_columns );
+
+		return $networks_columns;
+	}
+
+	function get_sortable_columns() {
+		return array(
+			'sitename'    => 'sitename',
+			'domain'      => 'domain',
+		);
+	}
+
+	function display_rows() {
+		global $current_site, $mode;
+
+		print_r($this->items);
+
+		$class = '';
+		foreach ( $this->items as $network ) {
+			$class = ( 'alternate' == $class ) ? '' : 'alternate';
+
+			echo "<tr class='$class'>";
+
+			list( $columns, $hidden ) = $this->get_column_info();
+
+			foreach ( $columns as $column_name => $column_display_name ) {
+				$style = '';
+				if ( in_array( $column_name, $hidden ) )
+					$style = ' style="display:none;"';
+
+				switch ( $column_name ) {
+					case 'cb': ?>
+						<th scope="row" class="check-column">
+							<input type="checkbox" id="network_<?php echo $network['site_id'] ?>" name="allnetworks[]" value="<?php echo esc_attr( $network['site_id'] ) ?>" />
+						</th>
+					<?php
+					break;
+
+					case 'domain':
+						echo "<td class='column-$column_name $column_name'$style>"; ?>
+							<?php echo $network['domain'] ?>
+						</td>
+					<?php
+					break;
+					
+					case 'path':
+						echo "<td class='column-$column_name $column_name'$style>"; ?>
+							<?php echo $network['path'] ?>
+						</td>
+					<?php
+					break;
+					
+					case 'sitename':
+						echo "<td class='column-$column_name $column_name'$style>"; 
+							
+						$siteurl = 'http://' . $network['domain'] . $network['blog_path'];
+						
+						$myurl = add_query_arg(array(
+							'mode'	=> false,
+							'page'	=> 'networks',
+							'id'	=> $network['id']
+						));
+						
+						?>
+						<a href="<?php echo add_query_arg(array( 'action'	=> 'editnetwork' ), $myurl ); ?>" class="edit"><?php echo $network['sitename']; ?></a>
+						<?php
+						
+						
+						if ( 'list' != $mode )
+							echo '<p></p>';
+
+						$actions = array(
+							'network_admin' 	=> '<span class="edit"><a href="' . $siteurl . 'wp-admin/network/" title="' . __( 'Network Admin' ) . '">' . __( 'Network Admin' ) . '</a></span>',
+							'assign_sites'		=> '<span class="edit"><a href="' . add_query_arg(array( 'action'	=> 'assignblogs' ), $myurl ) . '" title="' . __( 'Assign sites to this network' ) . '">' . __( 'Assign Sites' ) . '</a></span>',
+							'edit'				=> '<span class="edit"><a class="edit_network_link" href="' . add_query_arg(array( 'action'	=> 'editnetwork' ), $myurl ) . '" title="' . __( 'Edit this network' ) . '">' . __( 'Edit' ) . '</a></span>',
+						);
+						
+						if ( $current_site->id != $network['id'] && $network['id'] != 1 ) {
+							if ( current_user_can( 'manage_network_options', $network['id'] ) )
+								$actions['delete']	= '<span class="delete"><a href="' . esc_url( wp_nonce_url( add_query_arg(array( 'action'	=> 'deletenetwork' ), $myurl ) ) ) . '">' . __( 'Delete' ) . '</a></span>';
+						}
+
+						$actions = apply_filters( 'manage_networks_action_links', array_filter( $actions ), $network['id'], $network['sitename'] );
+						echo $this->row_actions( $actions );
+						?>
+						</td>
+					<?php
+					break;
+					
+					case 'blogs':
+						echo "<td valign='top' class='$column_name column-$column_name'$style>";
+							?>
+							<a href="http://<?php echo $network['domain'] . $network['blog_path']; ?>wp-admin/network/sites.php" title="<?php _e( 'Sites on this network' ); ?>"><?php echo $network['blogs'] ?></a>
+						</td>
+					<?php
+					break;
+					
+					
+					case 'admins':
+						echo "<td valign='top' class='$column_name column-$column_name'$style>";
+							$network_admins = maybe_unserialize( $network['network_admins']);
+							if( ! empty( $network_admins ) ) {
+								echo join( ', ', $network_admins );
+							}
+							?>
+						</td>
+					<?php
+					break;
+
+				case 'plugins': ?>
+					<?php if ( has_filter( 'wpmublogsaction' ) ) {
+					echo "<td valign='top' class='$column_name column-$column_name'$style>";
+						do_action( 'wpmublogsaction', $blog['blog_id'] ); ?>
+					</td>
+					<?php }
+					break;
+
+				default:
+					echo "<td class='$column_name column-$column_name'$style>";
+					do_action( 'manage_sites_custom_column', $column_name, $blog['blog_id'] );
+					echo "</td>";
+					break;
+				}
+			}
+			?>
+			</tr>
+			<?php
+		}
+	}
+}
+
+?>
--- wpmn-admin.php
+++ wpmn-admin.php
@@ -111,9 +111,13 @@ class WPMN_Admin {
 	}
 
 	function network_admin_menu() {
-		add_menu_page( __( 'Networks' ), __( 'Networks' ), 'manage_options', 'networks', array( &$this, 'networks_page' ), '', -1 );
+		$page = add_menu_page( __( 'Networks' ), __( 'Networks' ), 'manage_options', 'networks', array( &$this, 'networks_page' ), '', -1 );
 		add_submenu_page( 'networks', __( 'All Networks' ), __( 'All Networks' ), 'manage_options', 'networks',        array( &$this, 'networks_page' ) );
 		add_submenu_page( 'networks', __( 'Add New'      ), __( 'Add New'      ), 'manage_options', 'add-new-network', array( &$this, 'add_network_page' ) );
+		
+		require dirname(__FILE__) . '/includes/class-wp-ms-networks-list-table.php';
+		add_filter( 'manage_' . $page . '-network' . '_columns', array( new WP_MS_Networks_List_Table(), 'get_columns' ), 0 );
+		
 	}
 
 	/* Config Page */
@@ -201,335 +205,35 @@ class WPMN_Admin {
 	}
 
 	function all_networks() {
-		global $wpdb;
-
-		// Strip off the action tag
-		$query_str  = substr( $_SERVER['REQUEST_URI'], 0, ( strpos( $_SERVER['REQUEST_URI'], '?' ) + 1 ) );
-		$get_params = array();
-		$bad_params = array( 'action', 'id', 'updated', 'deleted' );
-		foreach ( $_GET as $get_param => $get_value ) {
-			if ( !in_array( $get_param, $bad_params ) ) {
-				$get_params[] = $get_param . '=' . $get_value;
-			}
-		}
-		$query_str .= implode( '&', $get_params );
-
-		$search_conditions = '';
-		if ( isset( $_GET['s'] ) ) {
-			if ( isset( $_GET['search'] ) && $_GET['search'] == __( 'Search Domains' ) ) {
-				$search_conditions = 'WHERE ' . $wpdb->site . '.domain LIKE ' . "'%" . $wpdb->escape( $_GET['s'] ) . "%'";
-			}
-		}
-
-		$count = $wpdb->get_col( $wpdb->prepare( 'SELECT COUNT(id) FROM ' . $wpdb->site . $search_conditions ) );
-		$total = $count[0];
-
-		if ( isset( $_GET['start'] ) == false ) {
-			$start = 1;
-		} else {
-			$start = intval( $_GET['start'] );
-		}
-
-		if ( isset( $_GET['num'] ) == false ) {
-			$num = NETWORKS_PER_PAGE;
-		} else {
-			$num = intval( $_GET['num'] );
-		}
-
-		$query =   "SELECT {$wpdb->site}.*, COUNT({$wpdb->blogs}.blog_id) as blogs, {$wpdb->blogs}.path as blog_path
-						FROM {$wpdb->site}
-					LEFT JOIN {$wpdb->blogs}
-						ON {$wpdb->blogs}.site_id = {$wpdb->site}.id {$search_conditions}
-						GROUP BY {$wpdb->site}.id";
-
-		if ( isset( $_GET['sortby'] ) ) {
-			$sortby = $_GET['sortby'];
-		} else {
-			$sortby = 'ID';
-		}
-
-		switch ( $sortby ) {
-			case 'Domain':
-				$query .= ' ORDER BY ' . $wpdb->site . '.domain ';
-				break;
-
-			case 'Path':
-				$query .= ' ORDER BY ' . $wpdb->site . '.path ';
-				break;
-
-			case 'Blogs':
-				$query .= ' ORDER BY blogs ';
-				break;
-
-			case 'ID':
-			default:
-				$query .= ' ORDER BY ' . $wpdb->site . '.id ';
-		}
-
-		if ( isset( $_GET['order'] ) && ( $_GET['order'] == 'DESC' ) ) {
-			$query .= 'DESC';
-		} else {
-			$query .= 'ASC';
-		}
-
-		$query .= ' LIMIT ' . ( ( (int) $start - 1 ) * $num ) . ', ' . intval( $num );
-
-		$site_list = $wpdb->get_results( $wpdb->prepare( $query ), ARRAY_A );
-
-		if ( count( $site_list ) < $num ) {
-			$next = false;
-		} else {
-			$next = true;
-		}
-
-		// define the columns to display, the syntax is 'internal name' => 'display name'
-		$sites_columns = array(
-			'check'  => '',
-			'domain' => __( 'Domain' ),
-			'id'     => __( 'Site ID' ),
-			'path'   => __( 'Path' ),
-			'blogs'  => __( 'Sites' ),
-		);
-		$sites_columns = apply_filters( 'manage_networks_columns', $sites_columns );
-
-		// Pagination
-		$network_navigation = paginate_links( array(
-			'base'    => add_query_arg( 'start', '%#%' ),
-			'format'  => '',
-			'total'   => ceil( $total / $num ),
-			'current' => $start
-		) ); ?>
-
-		<div id="icon-ms-admin" class="icon32"></div>
-		<h2><?php _e( 'Networks' ) ?></h2>
-
-		<div id="col-container">
-			<div class="tablenav">
-
-				<?php if ( isset( $_GET['s'] ) ) : ?>
-
-					<div class="alignleft">
-						<?php _e( 'Filter' ); ?>: <?php echo $wpdb->escape( $_GET['s'] ) ?> <a href="<?php echo $this->admin_url(); ?>" class="button" title="<?php _e( 'Remove this filter' ) ?>"><?php _e( 'Remove' ) ?></a>
-					</div>
-
-				<?php endif; ?>
-
-				<div class="alignright">
-					<form name="searchform" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="get">
-						<input type="text" name="s" />
-						<input type="hidden" name="page" value="networks" />
-						<input type="submit" name="search" id="search" class="button" value="<?php _e( 'Search Domains' ); ?>" />
-					</form>
-				</div>
-			</div>
-			<form name='formlist' action='<?php echo $_SERVER['REQUEST_URI'] . "&amp;action=delete_multinetworks"; ?>' method='POST'>
-				<div class="tablenav">
-
-					<?php if ( $network_navigation ) : ?>
-
-						<div class="tablenav-pages"><?php echo $network_navigation; ?></div>
-
-					<?php endif; ?>
-
-					<div class="alignleft">
-						<input type="submit" class="button-secondary delete" name="allnetwork_delete" value="<?php _e( 'Delete' ); ?>" />
-					</div>
-				</div>
-
-				<br class="clear" />
-
-				<table width="100%" cellpadding="3" cellspacing="3" class="widefat">
-					<thead>
-						<tr>
-
-							<?php foreach ( $sites_columns as $col_name => $column_display_name ) : ?>
-
-								<th scope="col"<?php if ( 'check' == $col_name ) : ?> class="column-cb check-column"<?php endif; ?>>
-
-									<?php if ( 'check' == $col_name ) : ?>
-
-											<input type="checkbox" id="select_all" />
-
-									<?php else : ?>
-
-										<?php
-
-											$url = add_query_arg( array (
-												'sortby' => urlencode( $column_display_name ),
-												'order'  => $_GET[ 'order' ],
-												'start'  => $start ),
-												$this->admin_url()
-											);
-
-										?>
-
-											<a href="<?php echo $url; ?>"><?php echo $column_display_name; ?></a>
-
-									<?php endif; ?>
-
-								</th>
-
-							<?php endforeach; ?>
-
-						</tr>
-					</thead>
-					<tfoot>
-						<tr>
-
-							<?php foreach ( $sites_columns as $col_name => $column_display_name ) : ?>
-
-								<th scope="col"<?php if ( 'check' == $col_name ) : ?> class="column-cb check-column"<?php endif; ?>>
-
-									<?php if ( 'check' == $col_name ) : ?>
-
-											<input type="checkbox" id="select_all" />
-
-									<?php else : ?>
-
-										<?php
-
-											$url = add_query_arg( array (
-												'sortby' => urlencode( $column_display_name ),
-												'order'  => $_GET[ 'order' ],
-												'start'  => $start ),
-												$this->admin_url()
-											);
-
-										?>
-
-											<a href="<?php echo $url; ?>"><?php echo $column_display_name; ?></a>
-
-									<?php endif; ?>
-
-								</th>
-
-							<?php endforeach; ?>
-
-						</tr>
-					</tfoot>
-					<tbody>
-
-						<?php
-
-						if ( $site_list ) {
-							$bgcolor = '';
-
-							foreach ( $site_list as $site ) { ?>
-
-							<tr>
-
-								<?php
-									if ( is_subdomain_install() )
-										$sitename = str_replace( '.' . $current_site->domain, '', $site['domain'] );
-									else
-										$sitename = $site['path'];
-
-									$cur_site_id  = (int) $site['id'];
-									$cur_site_url = esc_url( 'http://' . $site['domain'] . $site['path'] );
-
-									foreach ( $sites_columns as $column_name => $column_display_name ) {
-
-										switch ( $column_name ) {
-
-											case 'check' :
-												if ( $site['blogs'] == 0 || $site['id'] != 1 ) : ?>
-
-													<th scope="row" class="check-column" style="width: auto"><input type='checkbox' id='<?php echo $site[ 'id' ] ?>' name='allnetworks[]' value='<?php echo $site[ 'id' ] ?>' /><label for='<?php echo $cur_site_id ?>'></label></th>
-
-												<?php else : ?>
-
-													<th></th>
-
-												<?php endif;
-
-												break;
-
-											case 'id': ?>
-
-												<td><?php echo $cur_site_id ?></td>
-
-												<?php
-												break;
-
-											case 'domain': ?>
-
-												<td>
-													<label for="<?php echo $cur_site_id ?>"><?php echo $site['domain'] ?></label>
-													<?php
-													
-													$actions = array(
-//														'site_admin'		=> '<span class="edit"><a href="' . $cur_site_url . 'wp-admin/" title="' . __( 'Site Admin' ) . '">' . __( 'Site Admin' ) . '</a></span>',
-														'network_admin' 	=> '<span class="edit"><a href="' . $cur_site_url . 'wp-admin/network/" title="' . __( 'Network Admin' ) . '">' . __( 'Network Admin' ) . '</a></span>',
-														'assign_sites'		=> '<span class="edit"><a href="' . $query_str . '&amp;action=assignblogs&amp;id=' . $site['id'] . '" title="' . __( 'Assign sites to this network' ) . '">' . __( 'Assign Sites' ) . '</a></span>',
-														'edit'				=> '<span class="edit"><a class="edit_network_link" href="' . $query_str . '&amp;action=editnetwork&amp;id=' . $site['id'] . '" title="' . __( 'Edit this network' ) . '">' . __( 'Edit' ) . '</a></span>',
-													);
-													if( $site['id'] != 1 ) {
-														$actions['delete']	=  '<span class="delete"><a href="' . $query_str . '&amp;action=deletenetwork&amp;id=' .  $site['id'] . '" title="' . __( 'Delete this network' ) . '">' . __( 'Delete' ) . '</a></span>';
-													}
-													
-													?>
-													<?php if ( count( $actions ) ) : ?>
-													<div class="row-actions">
-														<?php echo implode( ' | ', $actions ); ?>
-													</div>
-													<?php endif; ?>
-												</td>
-
-												<?php
-												break;
-
-											case 'path': ?>
-
-												<td valign='top'><label for='<?php echo $cur_site_id ?>'><?php echo $site['path'] ?></label></td>
-
-												<?php
-												break;
-											case 'blogs': ?>
-
-												<td valign='top'><a href="http://<?php echo $site['domain'] . $site['blog_path']; ?>wp-admin/network/sites.php" title="<?php _e( 'Sites on this network' ); ?>"><?php echo $site['blogs'] ?></a></td>
-
-												<?php
-												break;
-
-											default: ?>
-
-												<td valign='top'><?php do_action( 'manage_networks_custom_column', $column_name, $site['id'] ); ?></td>
-
-												<?php
-												break;
-										}
-									}
-								?>
-							</tr>
-
-							<?php
-							}
-
-						} else { ?>
-
-						<tr style=''>
-							<td colspan="8"><?php _e( 'No networks found.' ) ?></td>
-						</tr>
-
-						<?php } ?>
-
-					</tbody>
-				</table>
-				<div class="tablenav">
-
-					<?php if ( $network_navigation ) : ?>
-
-						<div class="tablenav-pages"><?php echo $network_navigation; ?></div>
-
-					<?php endif; ?>
-
-					<div class="alignleft">
-						<input type="submit" class="button-secondary delete" name="allnetwork_delete" value="<?php _e( 'Delete' ); ?>" />
-					</div>
-				</div>
+		
+		$wp_list_table = new WP_MS_Networks_List_Table();
+		
+		$pagenum = $wp_list_table->get_pagenum();
+		$wp_list_table->prepare_items();
+
+		?>
+		<div class="wrap">
+			<?php screen_icon('ms-admin'); ?>
+			<h2><?php _e('Networks') ?>
+			<?php $this->feedback() ?>
+			<?php if ( current_user_can( 'manage_network_options' ) ) : ?>
+			        <a href="<?php echo $this->admin_url() ?>" class="add-new-h2"><?php echo esc_html_x( 'Add New', 'site' ); ?></a>
+			<?php endif; ?>
+			
+			<?php if ( isset( $_REQUEST['s'] ) && $_REQUEST['s'] ) {
+				printf( '<span class="subtitle">' . __( 'Search results for &#8220;%s&#8221;' ) . '</span>', esc_html( $s ) );
+			} ?>
+			</h2>
+			
+			<form action="" method="get" id="domain-search">
+				<?php $wp_list_table->search_box( __( 'Search Networks' ), 'networks' ); ?>
+				<input type="hidden" name="action" value="domains" />
+			</form>
+			
+			<form id="form-domain-list" action="admin.php?action=domains" method="post">
+				<?php $wp_list_table->display(); ?>
 			</form>
 		</div>
-
 		<?php
 	}
 
@@ -672,7 +376,7 @@ class WPMN_Admin {
 									<?php
 										$all_network_options       = $wpdb->get_results( $wpdb->prepare( 'SELECT DISTINCT meta_key FROM ' . $wpdb->sitemeta ) );
 										$known_networkmeta_options = network_options_to_copy();
-										$known_networkmeta_options = apply_filters( 'manage_networkmeta_descriptions', $known_networkmeta_options );
+										$known_networkmeta_options = apply_filters( 'manage_sitemeta_descriptions', $known_networkmeta_options );
 									?>
 
 									<td colspan="3">
@@ -817,7 +521,7 @@ class WPMN_Admin {
 				$sites = $_POST['from'];
 			}
 
-			$current_blogs = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->blogs} WHERE site_id=" . (int) $_GET['id'] ) );
+			$current_blogs = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM {$wpdb->blogs} WHERE site_id=%d", (int)$_GET['id'] ) );
 
 			foreach ( $sites as $site ) {
 				move_site( $site, (int) $_GET['id'] );
@@ -837,8 +541,8 @@ class WPMN_Admin {
 		} else {
 
 			// get network by id
-			$query = "SELECT * FROM {$wpdb->site} WHERE id=" . (int) $_GET['id'];
-			$network = $wpdb->get_row( $wpdb->prepare( $query ) );
+			$query = "SELECT * FROM {$wpdb->site} WHERE id=%d";
+			$network = $wpdb->get_row( $wpdb->prepare( $query, (int) $_GET['id'] ) );
 
 			if ( !$network )
 				die( __( 'Invalid network id.' ) );
@@ -849,7 +553,7 @@ class WPMN_Admin {
 
 			foreach ( $sites as $key => $site ) {
 				$table_name = $wpdb->get_blog_prefix( $site->blog_id ) . "options";
-				$site_name = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $table_name WHERE option_name='blogname'" ) );
+				$site_name = $wpdb->get_row( $wpdb->prepare( "SELECT * FROM $table_name WHERE option_name=%s", 'blogname' ) );
 
 				if ( !$site_name )
 					die( __( 'Invalid blog.' ) );
